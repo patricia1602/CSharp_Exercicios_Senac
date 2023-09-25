5a3777a8e849df5bba6ea15cc9148eba345fe131
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2023-09-25 14:09:56 -0300

    Exe5

---
+++ CSharp_Exe5_BuscaEmUmVetor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharp_Exe5_BuscaEmUmVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = { "Patrícia", "Josenildo", "Leonardo", "Manuela" };
+            string nomeBuscado = "Manuela";
+            bool encontrado = false;
+
+            foreach (string nome in nomes)
+            {
+                if (nome == nomeBuscado)
+                {
+                    encontrado = true;
+                    break;
+
+                }
+            }
+            if (encontrado)
+            {
+                Console.WriteLine("Nome encontrado!");
+            }
+            else
+            {
+                Console.WriteLine("Nome não encontrado.");
+            }
+        }
+    }
+}
