649153fd40e6ccabe3b363cf9ad1e9418f87f1a8
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2023-09-25 17:45:20 -0300

    Exe2

--- CSharp_Matriz_Exe1_SomaDeMatrizes/Program.cs
+++ CSharp_Matriz_Exe1_SomaDeMatrizes/Program.cs
@@ -6,6 +6,7 @@ namespace CSharp_Matriz_Exe1_SomaDeMatrizes
     {
         static void Main(string[] args)
         {
+         //Duas matrizes quadradas de ordem 3x3. Preencha essas matrizes com valores inteiros de sua escolha. Em seguida, crie uma terceira matriz que armazene a soma dos elementos correspondentes das duas matrizes anteriores.//
             int[,] matriz1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
             int[,] matriz2 = { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
             int[,] resultado = new int[3, 3];
---
+++ CSharp_Matriz_Exe2_MatrizIdentidade/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharp_Matriz_Exe2_MatrizIdentidade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crie uma matriz identidade de ordem 4x4. Uma matriz identidade é uma matriz quadrada onde os elementos da diagonal principal são 1 e os demais elementos são 0.//
+            int[,] matrizIdentidade = new int[4, 4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i == j)
+                    {
+                        matrizIdentidade[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrizIdentidade[i, j] = 0;
+                    }
+                    Console.WriteLine(matrizIdentidade[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
