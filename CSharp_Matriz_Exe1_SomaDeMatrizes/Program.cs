4be71b187239851906e786148f1d6eb68e5e6dc6
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2023-09-25 17:05:15 -0300

    Exe1

---
+++ CSharp_Matriz_Exe1_SomaDeMatrizes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp_Matriz_Exe1_SomaDeMatrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] matriz2 = { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
+            int[,] resultado = new int[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    resultado[i, j] = matriz1[i, j] + matriz2[i, j];
+                    Console.Write(resultado[i, j] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
