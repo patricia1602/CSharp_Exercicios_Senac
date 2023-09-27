b7029b2eb6c7211240bba1536df877a747bbc791
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2023-09-27 16:37:40 -0300

    Arquivos.Exe2

---
+++ CSharp_Arquivos_Exe2_ContadorDePalavras/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace CSharp_Arquivos_Exe2_ContadorDePalavras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o nome do arquivo: ");
+            string nomeDoArquivo = Console.ReadLine();
+
+            //verificar se arquivo existe
+            if (!File.Exists(nomeDoArquivo))
+            {
+                Console.WriteLine("Arquivo não encontrado!");
+                return;
+            }
+
+            //Ler o conteúdo do arquivo
+            string conteudo = File.ReadAllText(nomeDoArquivo);
+
+            // Dividir o conteúdo em palavras usando espaços como delimitadores
+            string[] palavras = conteudo.Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Exibir o número de palavras
+            Console.WriteLine($"O arquivo contém {palavras.Length} palavras.");
+
+        }
+    }
+}
