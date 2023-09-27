43bb389b82f7aefb3fc6c0b6a76cb161f37fa6fb
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2023-09-27 15:03:54 -0300

    Exe5

---
+++ CSharp_Matriz_Exe5_BuscaEmMatriz/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CSharp_Matriz_Exe5_BuscaEmMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare uma matriz retangular (qualquer tamanho) preenchida com números inteiros. Solicite ao usuário um valor para busca. Verifique se o valor está presente na matriz e, se estiver, imprima a posição (linha e coluna) onde o valor foi encontrado.//
+            int[,] matriz = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80, 90 } };
+            int valorBuscado;
+
+            Console.Write("Digite o valor a ser buscado na matriz: ");
+            if (int.TryParse(Console.ReadLine(), out valorBuscado))
+            {
+
+            }
+            bool encontrado = false;
+            int linhaEncontrada = -1;
+            int colunaEncontrada = -1;
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    if (matriz[i, j] == valorBuscado)
+                    {
+                        encontrado = true;
+                        linhaEncontrada = i;
+                        colunaEncontrada = j;
+                        break;
+                    }
+                }
+                if (encontrado)
+                {
+                    break;
+                }
+            }
+            if (encontrado)
+            {
+                Console.WriteLine("Valor encontrado na posição: Linha " + linhaEncontrada + ", Coluna" + colunaEncontrada);
+            }
+            else
+            {
+                Console.WriteLine("Valor não encontrado na matriz.");
+            }
+        }
+    }
+}
